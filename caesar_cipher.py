dcbc30b16cd1551d0a9de8ca42f68c69dab7f8e2
hmoore7676 <36486467+hmoore7676@users.noreply.github.com>
2018-02-15 13:22:29 -0500

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,21 @@
+def shift(letter, shift_amount):
+    MAX_KEY_SIZE = 126
+    unicode_value = ord(letter) + shift_amount
+    new_letter = chr(unicode_value)
+
+    return new_letter
+
+def encrypt(message, shift_amount):
+    result = ''
+    for letter in message:
+        result += shift(letter, shift_amount)
+
+    return result
+
+def decrypt(message, shift_amount):
+    return encrypt(message,-1 * shift_amount)
+
+secret_message = "helloO how are YOU!?~"
+encrypted_message = encrypt(secret_message, 3)
+
+print(encrypted_message)
